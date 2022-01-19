803a251d88bcea1a9f23f6d185fe3bee9a96a6f0
Pratyush Jha <61881158+Pratyush-svg@users.noreply.github.com>
2022-01-19 12:17:13 +0530

    update 2

--- index.py
+++ index.py
@@ -79,3 +79,31 @@ def plot_line(days, min_t, max_t):
     st.pyplot()
     plt.clf()
 
+def plot_bars(days, min_t, max_t):
+    # print(days)
+    days = dates.date2num(days)
+    rcParams['figure.figsize'] = 6, 4
+    min_temp_bar = plt.bar(days - 0.2, min_t, width=0.4, color='r')
+    max_temp_bar = plt.bar(days + 0.2, max_t, width=0.4, color='b')
+    plt.xticks(days)
+    x_y_axis = plt.gca()
+    xaxis_format = dates.DateFormatter('%d/%m')
+
+    x_y_axis.xaxis.set_major_formatter(xaxis_format)
+    plt.xlabel('Dates(dd/mm)')
+    plt.ylabel('Temperature')
+    plt.title('6-Day Weather Forecast')
+
+    for bar_chart in [min_temp_bar, max_temp_bar]:
+        for index, bar in enumerate(bar_chart):
+            height = bar.get_height()
+            xpos = bar.get_x() + bar.get_width() / 2.0
+            ypos = height
+            label_text = str(int(height))
+            plt.text(xpos, ypos, label_text,
+                     horizontalalignment='center',
+                     verticalalignment='bottom',
+                     color='black')
+    st.pyplot()
+    plt.clf()
+
